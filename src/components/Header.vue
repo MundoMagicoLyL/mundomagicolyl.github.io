e4b478a1c892b07f4ff5fd54ad9a956e9e32d52d
JesusLugo2002 <jesusdlugo02@gmail.com>
2024-12-23 20:25:41 +0000

    Progress on homepage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,13 +5,12 @@
 <template>
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <RouterLink to="/home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <img src="/logo.png" width="64">
         <span class="fs-4">Mundo Mágico L&L</span>
       </RouterLink>
 
       <ul class="nav nav-pills">
         <li class="nav-item"><RouterLink to="/home" class="nav-link">Home</RouterLink></li>
-        
       </ul>
     </header>
 </template>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>Homepage</h1>
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="/logo.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Manualidades y decoración de forma artesanal</h1>
+                <p class="lead">Desde la decoración de eventos hasta la creación de detallitos para esas personas o dias especiales, en <span class="text-success fw-bold">Mundo Mágico L&L</span> solo encontrarás productos hechos con amor y dedicación y servicios ofrecidos con la mayor atención.</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Contácta</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
